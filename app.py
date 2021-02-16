ebc2be503805470b4f7cbfe3fb8dec2112e04924
Nate <nathanperfetti@gmail.com>
2021-02-16 13:43:16 -0600

    heroku

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-sqlURL = os.environ.get('JAWSDB_URL')
+
+if os.environ.get('JAWSDB_URL'):
+        sqlURL = os.environ.get('JAWSDB_URL')
+else: 
+        sqlURL = 'mysql://trggzzxsal5pfuhy:uortzaowx45j5jtd@d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l6s0pv9wrx2b4z3b'
+
+
 from flask import Flask, render_template, request, redirect
+
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = sqlURL or 'mysql://trggzzxsal5pfuhy:uortzaowx45j5jtd@d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/l6s0pv9wrx2b4z3b'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = sqlURL
 
 db = SQLAlchemy(app)
 
